144112526f93b21e4360644b9ccd17b2940a6b4b
parkpeom94@gmail.com <parkpeom94@gmail.com>
2021-10-25 10:55:43 +0900

    주민번호 뒷자리에 첫자리가 1,3 이면 남자 2,4 이면 여자

--- Java문법/src/String_StringBuffer_Cutter/Test.java
+++ Java문법/src/String_StringBuffer_Cutter/Test.java
@@ -9,14 +9,14 @@ public class Test {
 		Scanner scanner = new Scanner(System.in);
 		
 		String jumin = scanner.nextLine();
-		
+		String sex = "";
 		// 940114-1
 		if(jumin.charAt(7) == '1' ||
 			 jumin.charAt(7) == '3' && jumin.length() == 13) {
-			System.out.println("남자입니다.");
+				sex = "남자";
 		} else if(jumin.charAt(7) == '2' 
 				|| jumin.charAt(7) == '4' && jumin.length() == 13) {
-			System.out.println("여자입니다.");
+				sex = "여자";
 		} else {
 			System.out.println("잘못입력함 ");
 		}
